5feabdfa0d4577a9353264ba52a8c5b680f209dd
niikhiljambhulkar <niikhil.jambhulkar@gmail.com>
2026-05-29 19:50:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-console.log("Portfolio Loaded");
+const roles = [
+    "Aspiring Data Analyst",
+    "Power BI Developer",
+    "Future AI Engineer",
+    "Future Software Developer"
+];
+
+let roleIndex = 0;
+let charIndex = 0;
+
+const typingElement = document.getElementById("typing");
+
+function typeEffect() {
+
+    if (charIndex < roles[roleIndex].length) {
+
+        typingElement.innerHTML += roles[roleIndex].charAt(charIndex);
+
+        charIndex++;
+
+        setTimeout(typeEffect, 100);
+
+    } else {
+
+        setTimeout(eraseEffect, 1500);
+    }
+}
+
+function eraseEffect() {
+
+    if (charIndex > 0) {
+
+        typingElement.innerHTML =
+            roles[roleIndex].substring(0, charIndex - 1);
+
+        charIndex--;
+
+        setTimeout(eraseEffect, 50);
+
+    } else {
+
+        roleIndex++;
+
+        if (roleIndex >= roles.length) {
+            roleIndex = 0;
+        }
+
+        setTimeout(typeEffect, 500);
+    }
+}
+
+typeEffect();
